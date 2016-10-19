30df130785f185cce5604144a379e8b0410b84c3
Huỳnh Hà <hahpse04181@fpt.edu.vn>
2016-10-19 21:55:06 +0700

    update

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from builtins import print
-from flask import Flask
+from flask import Flask, request
 import json
+
 app = Flask(__name__)
 
-
-post1 ={
-    "title " : "Gooday",
-    "content" :" Today I met a girl. She had black eyes and love ice - creams"
+post1 = {
+    "title ": "Gooday",
+    "content": " Today I met a girl. She had black eyes and love ice - creams"
 }
-post2 ={
-    "title" : "Bad day ever",
-    "content " : "Baday ever ever ever "
+post2 = {
+    "title": "Bad day ever",
+    "content ": "Baday ever ever ever "
 }
 print(post1["title "])
 print(post2["title"])
-posts =[post1,post2]
+posts = [post1, post2]
+
+
 @app.route('/')
 def hello_world():
     return json.dumps(posts)
 
 
+@app.route('/addpost',methods = ["POST"])
+def add_post():
+    args = request.form
+    title = args["title"]
+    content = args["content"]
+    new_post = {"title":title,"content":content}
+    print(title,content)
+    posts.append(new_post)
+    return "OK"
 if __name__ == '__main__':
     app.run()
