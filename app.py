800af5cf8fd2d03f246edd6b6f1093f12bac3a38
Huỳnh Hà <hahpse04181@fpt.edu.vn>
2016-10-19 20:46:07 +0700

    Update

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+from builtins import print
 from flask import Flask
-
+import json
 app = Flask(__name__)
 
 
+post1 ={
+    "title " : "Gooday",
+    "content" :" Today I met a girl. She had black eyes and love ice - creams"
+}
+post2 ={
+    "title" : "Bad day ever",
+    "content " : "Baday ever ever ever "
+}
+print(post1["title "])
+print(post2["title"])
+posts =[post1,post2]
 @app.route('/')
 def hello_world():
-    return 'Hello Techkids Android 5'
+    return json.dumps(posts)
 
 
 if __name__ == '__main__':
